1030024afbd2b7dc83cf374db07485ac11a62172
Kosuke Mizubayashi <miz@mizba.net>
2020-02-20 23:50:12 +0900

    Compact it a bit.

--- simple_sequencer/ui.h
+++ simple_sequencer/ui.h
@@ -35,54 +35,22 @@ enum {
 
 // Switch definition.
 enum {
-  sw0 = 0, 
-  sw1, 
-  sw2,
-  sw3,
-  sw4,
-  sw5,
-  sw6,
-  sw7,
-  sw8,
-  sw9,
+  sw0 = 0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9,
   sw_count
 };
 
 const uint8_t sw_pins[sw_count] = {
-  D34,
-  D35,
-  D36,
-  D37,
-  D38,
-  D40,
-  D41,
-  D42,
-  D46,
-  D47
+  D34, D35, D36, D37, D38, D40, D41, D42, D46, D47
 };
 
 // LED definition
 enum {
-  led0 = 0, 
-  led1, 
-  led2,
-  led3,
-  led4,
-  led5,
-  led6,
-  led7,
+  led0 = 0, led1, led2, led3, led4, led5, led6, led7,
   led_count
 };
 
 const uint8_t led_pins[led_count] = {
-  D16,
-  D17,
-  D18,
-  D19,
-  D22,
-  D23,
-  D24,
-  D25
+  D16, D17, D18, D19, D22, D23, D24, D25
 };
 
 // VR definition
